81d136980f93fffbaf07cd7990ce737c9372927b
Wang Siyuan <wsy0227@sjtu.edu.cn>
2026-01-28 20:45:14 +0800

    feat: add credits page and nav link

--- src/site.config.ts
+++ src/site.config.ts
@@ -48,6 +48,10 @@ export const menuLinks: { path: string; title: string }[] = [
 		path: "/notes/",
 		title: "Notes",
 	},
+	{
+		path: "/credits/",
+		title: "Credits",
+	},
 ];
 
 // https://expressive-code.com/reference/configuration/
