70ea7949f57a939eaa07879ebe6fe111e9e738c6
Wang Siyuan <wsy0227@sjtu.edu.cn>
2026-02-02 18:56:54 +0800

    chore(site): retitle to Siyuan's Personal Page

--- src/site.config.ts
+++ src/site.config.ts
@@ -14,7 +14,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	// Used as the default description meta property and webmanifest description
-	description: "An opinionated starter theme for Astro",
+	description: "Siyuan Wang’s personal page: research, publications, projects, and notes.",
 	// HTML lang property, found in src/layouts/Base.astro L:18 & astro.config.ts L:48
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -25,7 +25,7 @@ export const siteConfig: SiteConfig = {
 		- The link value found in src/components/layout/Header.astro L:35
 		- In the footer found in src/components/layout/Footer.astro L:12
 	*/
-	title: "Astro Cactus",
+	title: "Siyuan's Personal Page",
 	// ! Please remember to replace the following site property with your own domain, used in astro.config.ts
 	url: "https://astro-cactus.chriswilliams.dev/",
 };
