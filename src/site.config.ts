b34c4cb7481b75b3e014da21561ac68c2d7f7e0c
Wang Siyuan <wsy0227@sjtu.edu.cn>
2026-02-02 19:12:19 +0800

    feat(posts): show notes alongside posts and remove notes nav

--- src/site.config.ts
+++ src/site.config.ts
@@ -45,10 +45,6 @@ export const menuLinks: { path: string; title: string }[] = [
 		title: "Blog",
 	},
 	{
-		path: "/notes/",
-		title: "Notes",
-	},
-	{
 		path: "/publications/",
 		title: "Publications",
 	},
